417777f88c7dac9084cab82b2f78be38a86336a6
Shuuya <enderhoang@gmail.com>
2025-05-20 22:13:59 +0700

    up

--- assets/js/params.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(location.search);
-const searchTerm = params.get('search')?.toLowerCase() || '';
-const page = parseInt(params.get('page')) || 0;
-const animeId = params.get('anime');
--- assets/js/search.js
+++
@@ -1,8 +0,0 @@
-searchInput?.addEventListener('keydown', e => e.key === 'Enter' && goSearch());
-searchIcon ?.addEventListener('click', goSearch);
-searchReset?.addEventListener('click', () => { searchInput.value = ''; goSearch(); });
-
-function goSearch() {
-	const q = searchInput.value.trim();
-	location.href = q ? `?search=${encodeURIComponent(q)}` : location.pathname;
-}
